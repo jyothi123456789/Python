06e5d64dd21ec0ac938753869a65a926ae60a921
jyothi123456789 <139424720+jyothi123456789@users.noreply.github.com>
2023-11-09 16:24:13 +0530

    Add files via upload

---
+++ filehandling.1.py
@@ -0,0 +1,15 @@
+'''# Functions on File handling
+import os
+os.getcwd()
+#os.mkdir("C:\\Users\\navee\PycharmProjects\pythonProject\\venv\madhu")
+#print("Directory created")
+#os.chdir("C:\\Users\\navee\PycharmProjects\pythonProject\\venv\madhu")
+#print("Changed to Madhu direactory.")
+#os.rmdir("C:\\Users\\navee\PycharmProjects\pythonProject\\venv\madhu")
+#print("Madhu directory removed.")
+print("Create file using file handling functions.")
+madhu = open("C:\\Users\\navee\PycharmProjects\pythonProject\\venv\madhu.txt","w+")
+madhu.write('File created using file handling\nnew file created\nAccess mode is W+')
+madhu.read()
+
+'''
