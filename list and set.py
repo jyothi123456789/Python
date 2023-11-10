f2d98c4cebe477492f7bfe23712555609367943e
jyothi123456789 <139424720+jyothi123456789@users.noreply.github.com>
2023-11-10 16:48:20 +0530

    Add files via upload

---
+++ list and set.py
@@ -0,0 +1,5 @@
+original_list = [1, 2, 2, 3, 4, 4, 5]
+unique_set = set(original_list)
+unique_list = list(unique_set)
+print("Original list:", original_list)
+print("List with duplicates removed:", unique_list)
