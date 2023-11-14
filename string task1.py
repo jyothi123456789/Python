828e0b38607c2a5ca5f1d7f7fb41b0774c93dc5d
jyothi123456789 <139424720+jyothi123456789@users.noreply.github.com>
2023-11-14 17:31:44 +0530

    Add files via upload

---
+++ string task1.py
@@ -0,0 +1,24 @@
+# Function to create, concatenate, and print a string
+def string_operations():
+    # Creating a string
+    string1 = "Hello, "
+    string2 = "world!"
+
+    # Concatenating strings
+    concatenated_string = string1 + string2
+
+    # Printing the concatenated string
+    print("Concatenated String:", concatenated_string)
+
+    # Accessing substring
+    substring = concatenated_string[7:12]
+    print("Substring:", substring)
+
+# Calling the function to demonstrate string operations
+string_operations()
+
+
+
+
+
+
