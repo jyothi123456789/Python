27963a43503d19bc57a3da5b8d335b8145c39b3d
jyothi123456789 <139424720+jyothi123456789@users.noreply.github.com>
2023-11-14 17:30:49 +0530

    Add files via upload

---
+++ number data types.py
@@ -0,0 +1,43 @@
+# Integer demonstration
+integer_num = 42
+print("Integer:", integer_num, "Type:", type(integer_num))
+
+# Float demonstration
+float_num = 3.14
+print("Float:", float_num, "Type:", type(float_num))
+
+# Complex number demonstration
+complex_num = 2 + 3j
+print("Complex:", complex_num, "Type:", type(complex_num))
+
+# Arithmetic operations
+a = 10
+b = 3
+
+# Addition
+addition_result = a + b
+print("Addition:", addition_result)
+
+# Subtraction
+subtraction_result = a - b
+print("Subtraction:", subtraction_result)
+
+# Multiplication
+multiplication_result = a * b
+print("Multiplication:", multiplication_result)
+
+# Division
+division_result = a / b
+print("Division:", division_result)
+
+# Integer Division
+integer_division_result = a // b
+print("Integer Division:", integer_division_result)
+
+# Modulus
+modulus_result = a % b
+print("Modulus:", modulus_result)
+
+# Exponentiation
+exponentiation_result = a ** b
+print("Exponentiation:", exponentiation_result)
