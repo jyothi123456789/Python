cf8f290c6c8b7a517c40fcd8705aa0f7282c8cbc
jyothi123456789 <139424720+jyothi123456789@users.noreply.github.com>
2023-11-29 12:35:58 +0530

    Add files via upload

---
+++ panda nu fram.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+numArray = np.array([[1,2,3,4],[5,6,7,8]])
+DFObj = pd.DataFrame( numArray,index = ["one","two"]),columns=["subject1","1]
+print("the DF object:\n",DFObj)
