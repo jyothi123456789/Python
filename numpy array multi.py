0a6e232543e2c6f18beae0ec5353775b27579ddf
jyothi123456789 <139424720+jyothi123456789@users.noreply.github.com>
2023-12-02 22:53:08 +0530

    Add files via upload

---
+++ numpy array multi.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create a NumPy array
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Reshape the array
+reshaped_arr = arr.reshape(3, 2)
+print("Original Array:")
+print(arr)
+print("\nReshaped Array:")
+print(reshaped_arr)
