4d00f1540b0f9217c4f79e02b876a9e10d4a4884
jyothi123456789 <139424720+jyothi123456789@users.noreply.github.com>
2023-12-05 11:25:54 +0530

    Add files via upload

---
+++ array programs.py
@@ -0,0 +1,15 @@
+# Creating an array and printing its elements
+my_array = [1, 2, 3, 4, 5]
+print("Array elements:", my_array)
+
+# Accessing elements of an array
+print("First element:", my_array[0])
+print("Last element:", my_array[-1])
+
+# Modifying elements of an array
+my_array[2] = 10
+print("Modified array:", my_array)
+
+# Finding the length of an array
+array_length = len(my_array)
+print("Array length:", array_length)
