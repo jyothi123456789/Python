38328fde54f4db1954e81c0aabbb38c5be6b9431
jyothi123456789 <139424720+jyothi123456789@users.noreply.github.com>
2023-12-07 17:15:47 +0530

    Add files via upload

---
+++ reshaping.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Creating an array
+arr = np.array([1, 2, 3, 4, 5, 6])
+
+# Reshaping the array
+reshaped_arr = arr.reshape(2, 3)  # Reshape to a 2x3 matrix
+print("Reshaped Array:")
+print(reshaped_arr)
